fc920df49f8764c9ae9ed00acf1da5b334c1033b
gregjhansell97 <gregjhansell@gmail.com>
2019-07-10 14:06:38 -0400

    testing works

---
+++ tests/test_monitor_dict.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from pynoculars.monitor_dict import MonitorDict
+#from pynoculars.skeleton import fib
+
+__author__ = "gregjhansell97"
+__copyright__ = "gregjhansell97"
+__license__ = "mit"
+
+
+def test_fib():
+    pass
+
+#def test_fib():
+#    assert fib(1) == 1
+#    assert fib(2) == 1
+#    assert fib(7) == 13
+#    with pytest.raises(AssertionError):
+#        fib(-10)
